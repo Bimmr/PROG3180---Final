d37ec65a886b1641792ac5c6b6c205e4a4759e2c
Randy <bimmr6696@gmail.com>
2017-04-13 11:50:36 -0400

    Added Datebases

--- js/database.js
+++ js/database.js
@@ -21,30 +21,78 @@ var DB = {
 
         console.info("Create Tables ...");
 
+        //Create Book Table
         db.transaction(function (tx) {
-            var query = "";
+            var query = "CREATE TABLE IF NOT EXISTS Books (" +
+                "bookId             INTEGER      NOT NULL       PRIMARY KEY     AUTOINCREMENT," +
+                "bookName           VARCHAR(25)  NOT NULL," +
+                "bookAuthor         VARCHAR(25)  NOT NULL," +
+                "bookPublishDate    DATETIME             ," +
+                "bookTypeId         INTEGER      NOT NULL,   "
+                "FOREIGN KEY(bookTypeId) REFERENCES BookTypes(typeId));" +
+            ");";
+
+            tx.executeSql(query, [], function (tx, result) {
+                console.info("Success: Book Table created");
+            }, errorHandler);
+        }, errorHandler, successfulTransaction);
+
+        //Create Book Type
+        db.transaction(function (tx) {
+            var query = "CREATE TABLE IF NOT EXISTS BookTypes (" +
+                "typeId     INTEGER      NOT NULL       PRIMARY KEY     AUTOINCREMENT," +
+                "typeName   VARCHAR(25)  NOT NULL," +
+            ");";
 
             tx.executeSql(query, [], function (tx, result) {
                 console.info("Success: Type Table created");
+
+                //Add Default Types
+                db.transaction(function (tx) {
+                    var query = "INSERT INTO BookTypes (typeName) VALUES " +
+                        "(?)," +
+                        "(?);";
+                    var options = ['Fiction', 'Non-Fiction'];
+
+                    tx.executeSql(query, options, function (tx, result) {
+                        console.info("Success: Default Types Added");
+                    }, errorHandler);
+                }, errorHandler, successfulTransaction);
             }, errorHandler);
         }, errorHandler, successfulTransaction);
 
-
-    },
-
-    dropTable: function (table) {
-        console.info("Dropping Table ...");
+        //Create Review Table
         db.transaction(function (tx) {
-            var query = "DROP TABLE IF EXISTS " + table + ";"
+            var query = "CREATE TABLE IF NOT EXISTS Reviews (" +
+                "reviewId             INTEGER      NOT NULL       PRIMARY KEY     AUTOINCREMENT," +
+                "reviewName           VARCHAR(25)  NOT NULL," +
+                "reviewEmail          VARCHAR(25)  NOT NULL," +
+                "reviewDate           DATETIME     NOT NULL," +
+                "reviewTitle          VARCHAR(25)  NOT NULL," +
+                "reviewText           VARCHAR(100)  NOT NULL," +
+                "reviewVotes          INTEGER      NOT NULL,"
+                "bookId               INTEGER      NOT NULL,"
+                "FOREIGN KEY(bookId) REFERENCES Book(bookId));" +
+            ");";
 
             tx.executeSql(query, [], function (tx, result) {
-                console.info("Success: " + table + " Table dropped")
+                console.info("Success: Review Table created");
             }, errorHandler);
         }, errorHandler, successfulTransaction);
-    },
 
-    dropTables: function () {
-        DB.dropTable("tableName");
+        //Create SavedBooks Table
+        db.transaction(function (tx) {
+            var query = "CREATE TABLE IF NOT EXISTS SavedBooks (" +
+                "savedId             INTEGER      NOT NULL       PRIMARY KEY     AUTOINCREMENT," +
+                "savedURI           VARCHAR(50)  NOT NULL," +
+            ");";
+
+            tx.executeSql(query, [], function (tx, result) {
+                console.info("Success: SavedBooks Table created");
+            }, errorHandler);
+        }, errorHandler, successfulTransaction);
+
+
     }
 }
 
