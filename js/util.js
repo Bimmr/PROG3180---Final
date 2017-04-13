e1b6f1dd3331cec6d8e836fe3e9f80be3d75f267
Derek Fusari <derek.fusari@gmail.com>
2017-04-13 12:36:25 -0400

    Validate Search Complete

--- js/util.js
+++ js/util.js
@@ -1,4 +1,35 @@
 /**
- * Created by Randy on 04/8/17.
+ * Created by Randy, Derek on 04/8/17.
  */
+
+function doValidate_frmSearchBook() {
+    var form = $("#frmSearchBook");
+    form.validate({
+        rules: {
+            txtTitle: {
+                lengthCheck: true,
+                maxlength: 25
+            },
+            txtAuthor: {
+                lengthCheck: true,
+                maxlegnth: 25
+            }
+        }
+    });
+
+    jQuery.validator.addMethod("lengthCheck",
+        function (value, element) {
+
+            var valid = true;
+
+            if (value != null && value.length <= 2) {
+                valid = false
+            }
+            return valid;
+        },
+        "Please enter a title with more than 2 characters."
+    );
+
+}
+
 
