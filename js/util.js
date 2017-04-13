731073ae3c7dddc2fbf46663a05e27739b5530bd
Derek Fusari <derek.fusari@gmail.com>
2017-04-13 13:24:15 -0400

    Finished Validation in util. (Not Tested)

--- js/util.js
+++ js/util.js
@@ -13,9 +13,19 @@ function doValidate_frmSearchBook() {
             txtAuthor: {
                 lengthCheck: true,
                 maxlegnth: 25
+            },
+            messages:{
+                txtTitle:{
+                    lengthCheck: "Please enter a title with more than 2 characters.",
+                    maxlength: "Book title cannot be more than 25 characters"
+                },
+                txtAuthor:{
+                    lengthCheck: "Please enter an Author name with more than 2 characters.",
+                    maxlength: "Author name cannot be more than 25 characters"
+                }
             }
         }
-    });
+    },
 
     jQuery.validator.addMethod("lengthCheck",
         function (value, element) {
@@ -28,8 +38,61 @@ function doValidate_frmSearchBook() {
             return valid;
         },
         "Please enter a title with more than 2 characters."
-    );
+    ));
+}
 
+function doValidate_frmWriteReview(){
+    var form = $("#frmWriteReview");
+    form.validate({
+       rules:{
+           txtBookTitle: {
+               required: true,
+               minlength: 2,
+               maxlength: 25
+           },
+           txtUserName: {
+               required: true
+           },
+           txtEmail: {
+               required: true,
+               email: true,
+               emailcheck: true
+           },
+           txtReviewTitle: {
+               required: true
+           },
+           txtReviewText: {
+               required: true
+           },
+           messages:{
+                txtBookTitle: {
+                    required: "Please enter a book title",
+                    minlength: "Book title must be atleast 2 characters",
+                    maxlength: "Book title must be less than 25 characters"
+                },
+               txtUserName:{
+                    required: "Please enter a user name"
+               },
+               txtEmail:{
+                    required:"Please enter an email",
+                    emailcheck: "Please enter a valid email address"
+               },
+               txtReviewTitle:{
+                    requried:"Please enter a review title"
+               },
+               txtReviewText:{
+                    requried: "Please enter a review"
+               }
+           }
+       }
+    },
+        jQuery.validator.addMethod("emailcheck",
+            function (value, element) {
+                var regex = /^.+@/;
+                return this.optional(element) || regex.test(value);
+            },
+            "Please enter a valid email address."
+    ));
 }
 
 
