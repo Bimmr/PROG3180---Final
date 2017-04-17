fc891a19078cae930b972a981bd38bf6867c06ff
Randy <bimmr6696@gmail.com>
2017-04-17 01:23:00 -0400

    Fixed validations
    - Fixed spelling error
    - Fixed Messages not in right path

--- js/util.js
+++ js/util.js
@@ -7,12 +7,12 @@ jQuery.validator.addMethod("lengthCheck",
 
         var valid = true;
 
-        if (value != null && value.length <= 2)
+        if (value !== null && value !== "" && value.length <= 2)
             valid = false
 
         return valid;
     },
-    "Please enter a title with more than 2 characters."
+    "Please enter a value with more than 2 characters."
 );
 jQuery.validator.addMethod("emailcheck",
     function (value, element) {
@@ -27,24 +27,33 @@ function doValidate_frmSearchBookReview() {
     form.validate({
         rules: {
             txtSearchName: {
+                required: function () {
+                    return $("#txtSearchAuthor").val().length === 0;
+                },
                 lengthCheck: true,
                 maxlength: 25
             },
             txtSearchAuthor: {
-                lengthCheck: true,
-                maxlegnth: 25
-            },
-            messages: {
-                txtSearchTitle: {
-                    lengthCheck: "Please enter a title with more than 2 characters.",
-                    maxlength: "Book title cannot be more than 25 characters"
+                required: function () {
+                    return $("#txtSearchName").val().length === 0;
                 },
-                txtSearchAuthor: {
-                    lengthCheck: "Please enter an Author name with more than 2 characters.",
-                    maxlength: "Author name cannot be more than 25 characters"
-                }
+                lengthCheck: true,
+                maxlength: 25
+            }
+        },
+        messages: {
+            txtSearchName: {
+                required: "Fill in either the book title, or the author",
+                lengthCheck: "Please enter a title with more than 2 characters.",
+                maxlength: "Book title cannot be more than 25 characters"
+            },
+            txtSearchAuthor: {
+                required: "Fill in either the book title, or the author",
+                lengthCheck: "Please enter an Author name with more than 2 characters.",
+                maxlength: "Author name cannot be more than 25 characters"
             }
         }
+
     });
 
     return form.valid();
@@ -53,48 +62,49 @@ function doValidate_frmSearchBookReview() {
 function doValidate_frmWriteReview() {
     var form = $("#frmWriteReview");
     form.validate({
-            rules: {
-                txtBookTitle: {
-                    required: true,
-                    minlength: 2,
-                    maxlength: 25
-                },
-                txtUserName: {
-                    required: true
-                },
-                txtEmail: {
-                    required: true,
-                    email: true,
-                    emailcheck: true
-                },
-                txtReviewTitle: {
-                    required: true
-                },
-                txtReviewText: {
-                    required: true
-                },
-                messages: {
-                    txtBookTitle: {
-                        required: "Please enter a book title",
-                        minlength: "Book title must be atleast 2 characters",
-                        maxlength: "Book title must be less than 25 characters"
-                    },
-                    txtUserName: {
-                        required: "Please enter a user name"
-                    },
-                    txtEmail: {
-                        required: "Please enter an email",
-                        emailcheck: "Please enter a valid email address"
-                    },
-                    txtReviewTitle: {
-                        requried: "Please enter a review title"
-                    },
-                    txtReviewText: {
-                        requried: "Please enter a review"
-                    }
-                }
+        rules: {
+            txtBookTitle: {
+                required: true,
+                minlength: 2,
+                maxlength: 25
+            },
+            txtUserName: {
+                required: true
+            },
+            txtEmail: {
+                required: true,
+                email: true,
+                emailcheck: true
+            },
+            txtReviewTitle: {
+                required: true
+            },
+            txtReviewText: {
+                required: true
             }
-        });
+        },
+        messages: {
+            txtBookTitle: {
+                required: "Please enter a book title",
+                minlength: "Book title must be atleast 2 characters",
+                maxlength: "Book title must be less than 25 characters"
+            },
+            txtUserName: {
+                required: "Please enter a user name"
+            },
+            txtEmail: {
+                required: "Please enter an email",
+                emailcheck: "Please enter a valid email address"
+            },
+            txtReviewTitle: {
+                requried: "Please enter a review title"
+            },
+            txtReviewText: {
+                requried: "Please enter a review"
+            }
+
+        }
+    });
     return form.valid();
 
 }
