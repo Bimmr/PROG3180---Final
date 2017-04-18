9bce10774a05b8ca431398e052baa95b13ae0a99
Randy <bimmr6696@gmail.com>
2017-04-18 03:20:58 -0400

    Fixed bug with variable
    - Seperated code to prevent variable being overriden before it's current
    value got used
    - Added extra comments

--- js/global.js
+++ js/global.js
@@ -59,6 +59,8 @@ function init() {
             //TODO: Move this to facade
             $("#btnSearchBooks").on("click", function () {
                 if (doValidate_frmSearchBookReview()) {
+
+                    //Format fields and values into array
                     var fields = [];
                     var values = [];
                     if ($("#txtSearchName").val().length !== 0) {
@@ -88,47 +90,49 @@ function init() {
 
                                 //Iterate through all books
                                 for (var bRow = 0; bRow < bookResults.rows.length; bRow++) {
-
-                                    var bookRow = bookResults.rows[bRow];
-
-                                    //Get all reviews that match the books
-                                    Reviews.selectByField("bookId", bookRow.bookId,
-                                        function (tx, reviewResults) {
-
-
-                                            //Manually set classes/etc because it doesn't work nicely when you have multiple listviews being added at the same time, to the same div
-                                            var code = "<ul data-role='listview' data-inset='true' class='ui-listview ui-listview-inset ui-corner-all ui-shadow'>";
-                                            code += "<li data-role='list-divider' role='heading' class='ui-li-divider ui-bar-inherit'> <h1>" + bookRow.bookName + " (" + bookRow.bookAuthor + ")" + "</h1></li>";
-
-                                            //Iterate through all review
-                                            for (var revRow = 0; revRow < reviewResults.rows.length; revRow++) {
-                                                var reviewRow = reviewResults.rows[revRow];
-                                                code += "<li data-icon='none' class='ui-li-static ui-body-inherit'>";
-                                                code += "<p class='ui-li-aside'>" + reviewRow.reviewDate + "</p>";
-                                                code += "<h3>" + reviewRow.reviewTitle + "</h3>";
-                                                code += "<p>" + reviewRow.reviewText + "</p>";
-                                                code += "<p>" + reviewRow.reviewName + " - " + reviewRow.reviewEmail + "</p>";
-                                                code += "</li>";
-                                            }
-                                            code += "<li data-icon='plus'><a href='#' class='ui-btn ui-btn-icon-right ui-icon-plus'><h1>Add A New Review</h1></a></li>";
-                                            code += "</ul>";
-                                            element.append(code);
-
-                                        }, function () {
-                                            var code = "<ul data-role='listview' data-inset='true' class='ui-listview ui-listview-inset ui-corner-all ui-shadow'>";
-                                            code += "<li data-role='list-divider' role='heading' class='ui-li-divider ui-bar-inherit'> <h1>" + bookRow.bookName + " (" + bookRow.bookAuthor + ")" + "</h1></li>";
-                                            code += "<li data-icon='plus'><a href='#' class='ui-btn ui-btn-icon-right ui-icon-plus'><h1>Add A New Review</h1></a></li>";
-                                            code += "</ul>";
-                                            element.append(code);
-                                        }
-                                    );
+                                    getReviews(element, bookResults.rows[bRow]);
                                 }
-
                             } else {
                                 $.mobile.changePage("#pageReviewSearch");
                                 alert("No books were found with those details");
                             }
-                        }, errorHandler)
+                        }, errorHandler);
+
+                    //This has to be in a separate function to make sure bookRow isn't being overridden before it gets calls
+                    function getReviews(element, bookRow) {
+
+                        //Get all reviews that match the books
+                        Reviews.selectByField("bookId", bookRow.bookId,
+                            function (tx, reviewResults) {
+
+                                //Manually set classes/etc because it doesn't work nicely when you have multiple listviews being added at the same time, to the same div
+                                var code = "<ul data-role='listview' data-inset='true' class='ui-listview ui-listview-inset ui-corner-all ui-shadow'>";
+                                code += "<li data-role='list-divider' role='heading' class='ui-li-divider ui-bar-inherit'> <h1>" + bookRow.bookName + " (" + bookRow.bookAuthor + ")" + "</h1></li>";
+
+                                //Iterate through all review
+                                for (var revRow = 0; revRow < reviewResults.rows.length; revRow++) {
+                                    var reviewRow = reviewResults.rows[revRow];
+                                    code += "<li data-icon='none' class='ui-li-static ui-body-inherit'>";
+                                    code += "<p class='ui-li-aside'>" + reviewRow.reviewDate + "</p>";
+                                    code += "<h3>" + reviewRow.reviewTitle + "</h3>";
+                                    code += "<p>" + reviewRow.reviewText + "</p>";
+                                    code += "<p>" + reviewRow.reviewName + " - " + reviewRow.reviewEmail + "</p>";
+                                    code += "</li>";
+                                }
+                                code += "<li data-icon='plus'><a href='#' class='ui-btn ui-btn-icon-right ui-icon-plus'><h1>Add A New Review</h1></a></li>";
+                                code += "</ul>";
+                                element.append(code);
+
+                            },
+                            function () {
+                                var code = "<ul data-role='listview' data-inset='true' class='ui-listview ui-listview-inset ui-corner-all ui-shadow'>";
+                                code += "<li data-role='list-divider' role='heading' class='ui-li-divider ui-bar-inherit'> <h1>" + bookRow.bookName + " (" + bookRow.bookAuthor + ")" + "</h1></li>";
+                                code += "<li data-icon='plus'><a href='#' class='ui-btn ui-btn-icon-right ui-icon-plus'><h1>Add A New Review</h1></a></li>";
+                                code += "</ul>";
+                                element.append(code);
+                            }
+                        );
+                    }
                 }
             });
         }
