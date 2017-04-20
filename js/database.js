77b381338430fd3e21cff9f44454139ce373091b
Derek Fusari <derek.fusari@gmail.com>
2017-04-20 13:26:54 -0400

    Added real reviews

--- js/database.js
+++ js/database.js
@@ -146,9 +146,14 @@ function addDataToDatabase() {
 
     db.transaction(function (tx) {
         var query = "INSERT INTO Reviews VALUES " +
-            "(NULL, 'PersonA@Email.com', '2/2/2017', 'Review Title', 'This is the review text', 0, 1)," +
-            "(NULL, 'PersonB@Email.com', '2/2/2017', 'ReviewB Title', 'This is the review B text', 2, 1)," +
-            "(NULL, 'PersonA@Email.com', '2/2/2017', 'ReviewC Title', 'This is the review C text', 5, 2);";
+            "(NULL, 'jsmith@yahoo.ca', '3/6/2016', 'Classic!', 'I loved this novel! It's such a good read, "+
+                "definitely worth looking in to.', 5, 1)," +
+            "(NULL, 'edouglas@gmail.com', '2/12/2017', 'It was confusing', 'I found the story difficult "+
+                "to follow. Too many different plot points', 2, 3)," +
+            "(NULL, 'dfusari@gmail.com', '8/18/2017', 'Okay.', 'Not very interesting."+
+                "Couldn't finish it because I got too bored', 0, 4)," +
+            "(NULL, 'rbimmA@gmail.com', '12/25/2018', 'Excellent read', 'A classic book to curl up and read over christmas holidays', 5, 1);";
+
 
         tx.executeSql(query, [], function (tx, result) {
             console.info("Success: Default Types Added");
